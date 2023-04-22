75d43c6611c84817fca26510654869f7a4da357a
Jackie Chen <ecuojackie@yahoo.com>
2023-04-21 18:34:40 -0700

    Fix sendgrid API key and forwarding email

--- pages/api/sendgrid.js
+++ pages/api/sendgrid.js
@@ -4,19 +4,19 @@ sendgrid.setApiKey(process.env.NEXT_PUBLIC_SENDGRID_API_KEY);
 
 async function sendEmail(req, res) {
 	try {
-		// console.log("REQ.BODY", req.body);
+		//console.log("REQ.BODY", req.body);
 		await sendgrid.send({
-			to: "ecuojackie@gmail.com", // Your email where you'll receive emails
-			from: "ecuojackie@gmail.com", // your website email address here
+			to: "support@jackiedev.com", // Your email where you'll receive emails
+			from: "support@jackiedev.com", // your website email address here
 			subject: `Lead from portfolio website: ${req.body.subject}`,
 			html: `<div>
                 <h2> You received a messasge from ${req.body.name}, their email is ${req.body.email}. </h2>
                 <br/>
                 <h3> Message: ${req.body.message} </h3>
             </div>`,
-		});
+		})
 	} catch (error) {
-		// console.log(error);
+		//console.log(error);
 		return res
 			.status(error.statusCode || 500)
 			.json({ error: error.message });
